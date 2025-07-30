29c4b954fd9e80a9d0db18ec692edf4f276cc66e
Stephan Schröter <stephan.schroeter@me.com>
2025-07-30 16:48:03 +0200

    fix(react-router): improve a11y | sas / hj

--- react-router/app/routes/home.tsx
+++ react-router/app/routes/home.tsx
@@ -2,7 +2,7 @@ import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 import { PWordmark } from "@porsche-design-system/components-react/ssr";
 
-export function meta({}: Route.MetaArgs) {
+export function meta(_: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
