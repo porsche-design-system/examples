ab4d644753328c666e45be35261ba776e0dcf7b9
Stephan Schröter <stephan.schroeter@me.com>
2025-07-31 14:03:53 +0200

    fix(react-router): fix base ref | sas / hj

--- react-router/app/root.tsx
+++ react-router/app/root.tsx
@@ -5,41 +5,11 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-  useRouteLoaderData,
 } from "react-router";
 
 import type { Route } from "./+types/root";
 import "./app.css";
-import {
-  getComponentChunkLinks,
-  getFontFaceStyles,
-  getFontLinks,
-  getIconLinks,
-  getInitialStyles,
-  getMetaTagsAndIconLinks,
-} from '@porsche-design-system/components-react/partials';
 import { PorscheDesignSystemProvider } from '@porsche-design-system/components-react/ssr';
-
-export async function loader() {
-  return {
-    headPartials: (
-      <>
-        {/* necessary for SSR support, injects stylesheet which defines visibility of pre-hydrated PDS components */}
-        {getInitialStyles({ format: 'jsx' })}
-        {/* injects stylesheet which defines Porsche Next CSS font-face definition (=> minimize FOUT) */}
-        {getFontFaceStyles({ format: 'jsx' })}
-        {/* preloads Porsche Next font (=> minimize FOUT) */}
-        {getFontLinks({ format: 'jsx' })}
-        {/* preloads PDS component core chunk from CDN for PDS component hydration (=> improve loading performance) */}
-        {getComponentChunkLinks({ format: 'jsx' })}
-        {/* preloads Porsche icons (=> minimize FOUC) */}
-        {getIconLinks({ format: 'jsx' })}
-        {/* injects favicon, apple touch icons, android touch icons, etc. */}
-        {getMetaTagsAndIconLinks({ appTitle: 'Porsche', format: 'jsx' })}
-      </>
-    ),
-  };
-}
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -55,8 +25,6 @@ export const links: Route.LinksFunction = () => [
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  const partials = useRouteLoaderData<typeof loader>('root');
-
   return (
     <html lang="en">
       <head>
@@ -64,7 +32,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
-        {partials?.headPartials}
       </head>
       <body>
         {children}
--- react-router/react-router.config.ts
+++ react-router/react-router.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
+  basename: '/examples/react-router/',
   ssr: false, // disable SSR
   prerender: true, // enable SSG
 } satisfies Config;
