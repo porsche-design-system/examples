9c40e7670ad072617b9fc80de4818cf52b809cb0
Stephan Schröter <stephan.schroeter@me.com>
2025-08-05 15:07:05 +0200

    fix(react): lint errors | sas / hj

--- frameworks/react/src/App.tsx
+++ frameworks/react/src/App.tsx
@@ -14,16 +14,16 @@ function App() {
         <h1 className="prose-display-md">Porsche Design System</h1>
       </div>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank" rel="noopener">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noopener">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button type="button" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
--- frameworks/react/src/main.tsx
+++ frameworks/react/src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import { PorscheDesignSystemProvider } from '@porsche-design-system/components-react';
 
+// biome-ignore lint/style/noNonNullAssertion: ok
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <PorscheDesignSystemProvider>
