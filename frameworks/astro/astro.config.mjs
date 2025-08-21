0216c80623117c644835a0c45cce0849341481c7
Stephan Schröter <stephan.schroeter@me.com>
2025-08-21 15:43:41 +0200

    feat(frameworks/astro): add ci build | sas / hj

--- frameworks/astro/astro.config.mjs
+++ frameworks/astro/astro.config.mjs
@@ -5,6 +5,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  base: process.env.ASTRO_PUBLIC_BASE_PATH || '',
   integrations: [
     vue({
       template: {
