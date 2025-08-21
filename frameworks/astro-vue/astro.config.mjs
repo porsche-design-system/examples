d07084a703447f99e447154145afff9162d6adbc
Stephan Schröter <stephan.schroeter@me.com>
2025-08-21 15:00:14 +0200

    feat(frameworks/astro-vue): add pds component | sas / hj

--- frameworks/astro-vue/astro.config.mjs
+++ frameworks/astro-vue/astro.config.mjs
@@ -1,15 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 
 import vue from '@astrojs/vue';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
 });
