59a582fdb9de1063401e8c2f6de13e84935f37a0
Stephan Schröter <stephan.schroeter@me.com>
2025-08-21 11:07:47 +0200

    feat: improve linter config | sas / oh

--- patterns/src/ai-tag/1/main.js
+++ patterns/src/ai-tag/1/main.js
@@ -1,13 +1,1 @@
 import './style.css';
-
-// DO NOT USE IN PRODUCTION!
-// EXAMPLE CODE FOR DEMONSTRATION PURPOSE ONLY.
-const pPopover = document.querySelectorAll('p-popover');
-
-for (const el of pPopover) {
-  const button = el.querySelector('button');
-  button.addEventListener('click', (e) => {
-    el.open = !el.open;
-    e.target.setAttribute('aria-expanded', el.open);
-  });
-}
