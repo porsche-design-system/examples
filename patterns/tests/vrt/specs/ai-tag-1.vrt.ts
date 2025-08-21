84ec9455c0c1fd7fa472d039dfd597805e3943b3
Stephan Schröter <stephan.schroeter@me.com>
2025-08-21 11:58:20 +0200

    test(patterns): add vrt test for ai-tag | sas / oh

--- patterns/tests/vrt/specs/ai-tag-1.vrt.ts
+++ patterns/tests/vrt/specs/ai-tag-1.vrt.ts
@@ -1,21 +1,12 @@
 import { expect, test } from '@playwright/test';
 import { enableForcedColors, enableRightToLeft, enableTextZoom } from '../utils';
 
-const pattern = 'header-1';
+const pattern = 'ai-tag-1';
 
 test.describe(`has no visual regression "${pattern}"`, () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto('/examples/patterns/header/1/');
-
+    await page.goto('/examples/patterns/ai-tag/1/');
     await expect(page.locator('html')).toHaveClass(/hydrated/);
-
-    await page.locator('video').evaluate((video: HTMLVideoElement): void => {
-      video.pause();
-      video.currentTime = 0;
-    });
-
-    // TODO: wait for video
-    await new Promise((resolve) => setTimeout(resolve, 100));
   });
 
   test('default', async ({ page }) => {
