32c57c57d0d86feb3b97d441e6c1e5eb9fffcb71
Stephan Schröter <stephan.schroeter@me.com>
2025-10-09 13:11:29 +0200

    test(astro): improve e2e test | sas

--- frameworks/astro/tests/e2e/specs/app.e2e.ts
+++ frameworks/astro/tests/e2e/specs/app.e2e.ts
@@ -5,4 +5,5 @@ test('has title', async ({ page }) => {
 
   await expect(page).toHaveTitle('Astro Basics');
   await expect(page.getByRole('heading', { name: 'Porsche Design System' }).first()).toBeVisible();
+  await expect(page.locator('p-wordmark').first()).toBeVisible();
 });
