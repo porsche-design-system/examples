0759bc4704eb1bfe559d0381b520c465febca4c7
Stephan Schröter <stephan.schroeter@me.com>
2025-10-09 12:25:21 +0200

    feat(astro): integrate tailwind css | sas

--- frameworks/astro/astro.config.mjs
+++ frameworks/astro/astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 
 import vue from '@astrojs/vue';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
@@ -15,4 +16,8 @@ export default defineConfig({
       },
     }),
   ],
+  vite: {
+    // @ts-expect-error: The types are currently mismatched due to conflicting Vite versions (Astro and the Tailwind CSS Vite plugin are using different versions of Vite)
+    plugins: [tailwindcss()],
+  },
 });
