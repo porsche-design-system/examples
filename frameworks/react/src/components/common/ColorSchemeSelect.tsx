e892ad64f0c3a94e1a93e6f28c20a27048b86c5d
Stephan Schröter <stephan.schroeter@porsche.digital>
2026-03-26 14:27:44 +0100

    fix(react): migrate select from theme to color-scheme | sas

--- frameworks/react/src/components/common/ColorSchemeSelect.tsx
+++ frameworks/react/src/components/common/ColorSchemeSelect.tsx
@@ -6,12 +6,12 @@ import {
 } from '@porsche-design-system/components-react';
 import type { ColorScheme } from '../../models/colorScheme.ts';
 
-type ThemeSelectProps = {
+type ColorSchemeSelectProps = {
   value: ColorScheme;
   onChange: (event: CustomEvent<SelectChangeEventDetail>) => void;
 } & Partial<PSelectProps>;
 
-export const ColorSchemeSelect = ({ value = 'scheme-light-dark', onChange, ...rest }: ThemeSelectProps) => {
+export const ColorSchemeSelect = ({ value = 'scheme-light-dark', onChange, ...rest }: ColorSchemeSelectProps) => {
   return (
     <PSelect name="color-scheme" value={value} onChange={onChange} label="Color Scheme" compact={true} {...rest}>
       <PSelectOption value="scheme-light">Light</PSelectOption>
